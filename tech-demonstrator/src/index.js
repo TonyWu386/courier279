4ad54b945048c316f83dc566f90ec2e1ca00f377
UsernameOfAlexT <alexander.tam@mail.utoronto.ca>
2019-03-11 19:32:45 -0400

    Remove unused clutter

--- tech-demonstrator/src/index.js
+++ tech-demonstrator/src/index.js
@@ -6,78 +6,8 @@ import util from 'tweetnacl-util';
 
 
 import SceneController from './SceneController.js';
+import SceneTxtCtrl from './SceneTxtController.js';
 import LoginPortal from './LoginPortal';
-
-
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
-    {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-
-
-
-
-class Board extends React.Component {
-  
-  renderSquare(i) {
-    return <Square value={this.props.squares[i]}
-             onClick={() => this.props.onClick(i)}
-             />;
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-
-
-
 
 
 class PubKeyObj extends React.Component {
@@ -183,10 +113,7 @@ class Webapp extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
+    
     squares[i] = (this.state.xIsNext)? 'X' : 'O';
     this.setState({
       history: history.concat([{
@@ -211,8 +138,6 @@ class Webapp extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
-    const winner = calculateWinner(current.squares);
 
     const keypair = nacl.box.keyPair();
     console.log(keypair);
@@ -229,11 +154,6 @@ class Webapp extends React.Component {
     });
 
     let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + ((this.state.xIsNext)? 'X' : 'O');
-    }
 
     let cryptResult = this.state.cryptResult;
 
@@ -249,14 +169,11 @@ class Webapp extends React.Component {
           <PubKeyObj/>
         </div>
         <h4>Outside Crypt Result: {cryptResult}</h4>
-        <div className="game-board">
-          <Board 
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
         <div className="threejs-canvas">
           <SceneController/>
+        </div>
+        <div className="threejs-text">
+          <SceneTxtCtrl/>
         </div>
         <div className="game-info">
           <div>{status}</div>
